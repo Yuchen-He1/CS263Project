b07bbe7d1040cafbcac0e193e5f3be32ceae4179
rifatbit0401 <rifat92iitdu@gmail.com>
2025-02-13 21:19:19 -0800

    added java code in the pipline to calculate native java execution time

--- java-pipeline/Main.java
+++ java-pipeline/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String args[]) {
+        long startTime = System.nanoTime();
+        testFunction();
+        long endTime = System.nanoTime();
+        System.out.println("Execution Time: " + (double)(endTime - startTime)/1000000 + " ms");
+    }
+
+    public static void testFunction() {
         System.out.println("Hello world from Java");
     }
 
